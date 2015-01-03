7835c1c0591e6785b2a04f12adda65ad96ae11c7
etaf <fjdksfjdks@qq.com>
2015-01-03 22:25:59 +0800

    add solutions

--- leetcode/subset.cpp
+++ leetcode/subset.cpp
@@ -14,15 +14,26 @@
 #include<cmath>
 #include<cstdlib>
 using namespace std;
+
 class Solution {
 public:
     vector<vector<int> > subsets(vector<int> &S) {
         sort(S.begin(),S.end());
         int n = S.size();
-        vector<int> path(n,0);
         vector<vector<int> > ans;
-        int i=0;
-        while
+        int m = (1<<(n));
+        int x;
+        for(int i=0;i<m;++i){
+            x = i;
+            vector<int> path;
+            for(int j=0;j<n;++j){
+                if(x&1)path.push_back(S[j]);
+                x >>=1;
+            }
+            ans.push_back(path);
+        }
+        return ans;
+    }
 };
 
 class Solution_recursive {
@@ -53,15 +64,11 @@ public:
 int main()
 {
     Solution sol;
-    sol.subsets
     vector<int> S({1,2,3});
     vector<vector<int> >ans  = sol.subsets(S);
-    sol.subsetsfdsa<Plug>delimitMateBS<Plug>delimitMateBS<Plug>delimitMateBS<Plug>delimitMateBS<Plug>delimitMateBS<Plug>delimitMateBS
     for(int i=0;i<ans.size();++i){
         for(int j=0;j<ans[i].size();++j) cout<<ans[i][j]<<" ";
         cout<<endl;
     }
     return 0;
 }
-
-
