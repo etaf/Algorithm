4315a0c536db65c8c3b7dc73b901524a598ef169
etaf <fjdksfjdks@qq.com>
2015-03-12 19:21:19 +0800

    add solutions

---
+++ leetcode/number-of-1-bits.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<map>
+#include<set>
+#include<stack>
+#include<queue>
+#include<limits.h>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<cstdlib>
+using namespace std;
+class Solution {
+    public:
+        int hammingWeight(uint32_t n) {
+            int res = 0;
+            while(n){
+                res += (n&1)
+            }
+        }
+}
+int main()
+{
+    return 0;
+}
+
+
